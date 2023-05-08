b87c67ca89a5229638723708925c48e8e9a47408
Luciana Monteiro Cedro <luciana_cedro@hotmail.com>
2023-05-08 09:55:05 -0300

    incluindo mask phone e tornando textarea fixed

--- contato-script.js
+++ contato-script.js
@@ -1,3 +1,7 @@
+$(document).ready(function(){
+  $('#telefone').inputmask('99 99999-9999');
+});
+
 const form = document.querySelector("#contact-form");
 const successModal = document.querySelector("#success-modal");
 
