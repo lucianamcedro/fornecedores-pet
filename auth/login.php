e4385dabd242e1832f21fb56167865cdb72219b6
Luciana Cedro <luciana_cedro@hotmail.com>
2023-06-26 22:42:45 -0300

    ajuste layout

--- auth/login.php
+++ auth/login.php
@@ -1,50 +1,53 @@
-
-
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
-    <link rel="stylesheet" href="../css/header.css">
-    <link rel="icon" type="image/png" href="./img/ico.jpg">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/login.css">
+    <link rel="icon" type="image/png" href="/img/ico.jpg">
     <title>Login</title>
 </head>
 
 <body>
-    <div class="container-meio">
-        <div class="container-inicio">
-            <header>
-                <h1>
-                    <a href="../index.php" class="logo">AuPet<span class="span">.</span></a>
-                </h1>
-                <div class="header-link">
-                    <li>
-                        <a href="../index.php">Inicio</a>
-                    </li>
-                    <li>
-                        <a href="../contato.php" class="contato">Contato</a>
-                    </li>
-                    <li>
-                        <a href="../fornecedores.php" class="fornecedores">Fornecedores</a>
-                    </li>
-                    <li>
-                        <a href="#login" class="login">Login</a>
-                    </li>
+    <div class="container-inicio">
+        <header>
+            <h1>
+                <a href="../index.php" class="logo">AuPet<span class="span">.</span></a>
+            </h1>
+            <div class="header-link">
+                <li>
+                    <a href="../index.php">Inicio</a>
+                </li>
+                <li> <a href="../contato.php" class="contato">Contato</a></li>
+                <li><a href="../fornecedores.php" class="fornecedores">Fornecedores</a></li>
+            </div>
+        </header>
+        <div class="container-login">
+
+            <form method="POST" action="processar-login.php">
+
+
+                <div style="display: inline-block; width: calc(100%);">
+                    <label for="email">Email:</label>
+                    <input type="text" class="form-control" id="email" name="email" placeholder="Digite seu email"
+                        style="width: 100%;" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required>
                 </div>
-            </header>
+                <div style="display: inline-block; width: calc(100%);">
+                    <label for="telefone">Senha:</label>
+                    <input type="password" name="senha" class="form-control" name="phone" style="width:
+                            100%;">
+                </div>
 
-            <h2>Tela de Login</h2>
-    <form method="POST" action="processar-login.php">
-        <label for="nome">Nome de usuário:</label>
-        <input type="text" id="nome" name="nome" required><br><br>
+                <div style="display: inline-block; width: calc(100%);">
+                    <button type="submit" class="btn btn-primary" style="width:
+                            100%;">Entrar</button>
+                </div>
+            </form>
+        </div>
+        <script src="../script/contato-script.js"></script>
 
-        <label for="senha">Senha:</label>
-        <input type="password" id="senha" name="senha" required><br><br>
-
-        <input type="submit" value="Entrar">
-    </form>
-        </div> 
-    </div>
 </body>
+
 </html>
--- cadastro/novo-usuario.php
+++ cadastro/novo-usuario.php
@@ -1,59 +1,60 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
-    <link rel="stylesheet" href="../css/header.css">
-    <link rel="icon" type="image/png" href="./img/ico.jpg">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/cadastrar.css">
+    <link rel="icon" type="image/png" href="/img/ico.jpg">
     <title>Cadastrar</title>
 </head>
-
 <body>
-    <div class="container-meio">
-        <div class="container-inicio">
-            <header>
-                <h1>
-                    <a href="../index.php" class="logo">AuPet<span class="span">.</span></a>
-                </h1>
-                <div class="header-link">
-                    <li>
-                        <a href="../index.php">Inicio</a>
-                    </li>
-                    <li>
-                        <a href="../contato.php" class="contato">Contato</a>
-                    </li>
-                    <li>
-                        <a href="../fornecedores.php" class="fornecedores">Fornecedores</a>
-                    </li>
-                    <li>
-                        <a href="#login" class="login">Login</a>
-                    </li>
-                </div>
-            </header>
-
+    <div class="container-inicio">
+        <header>
+            <h1>
+                <a href="../index.php" class="logo">AuPet<span class="span">.</span></a>
+            </h1>
+            <div class="header-link">
+                <li>
+                    <a href="../index.php">Inicio</a>
+                </li>
+                <li> <a href="../contato.php" class="contato">Contato</a></li>
+                <li><a href="../fornecedores.php" class="fornecedores">Fornecedores</a></li>
+            </div>
+        </header>
+        <div class="container-cadastrar">
             <form action="salvar-usuario.php" method="POST">
-                <div class="mb-3">
-                    <label>Nome</label>
-                    <input type="text" name="nome" class="form-control">
-                </div>
-                <div class="mb-3">
-                    <label>Email</label>
-                    <input type="email" name="email" class="form-control">
-                </div>
-                <div class="mb-3">
-                    <label>Senha</label>
-                    <input type="password" name="senha" class="form-control">
-                </div>
-                <div class="mb-3">
-                    <label>Data de nascimento</label>
-                    <input type="date" name="data_nascimento" class="form-control">
-                </div>
-                <div class="mb-3">
-                    <button type="submit" class="btn btn-primary">Enviar</button>
-                </div>
-            </form>
-        </div> 
-    </div>
+                <form action="salvar-usuario.php" method="POST">
+
+                <div style="display: inline-block; width: calc(100%);">
+                        <label for="nome">Nome:</label>
+                        <input type="text" class="form-control" id="nome" name="name" placeholder="Digite seu nome"
+                            style="width: 100%;">
+                    </div>
+                    <div style="display: inline-block; width: calc(100%);">
+                        <label>Data de nascimento</label>
+                        <input type="date" name="data_nascimento" class="form-control"  style="width: 100%;>
+                    </div>
+                    <div style="display: inline-block; width: calc(100%);">
+                        <label for="email">Email:</label>
+                        <input type="text" class="form-control" id="email" name="email" placeholder="Digite seu email"
+                            style="width: 100%;" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required>
+                    </div>
+                    <div style="display: inline-block; width: calc(100%);">
+                        <label for="telefone">Senha:</label>
+                        <input type="password" name="senha" class="form-control" name="phone" style="width:
+                            100%;">
+                    </div>
+
+                    <div style="display: inline-block; width: calc(100%);">
+                        <button type="submit" class="btn btn-primary" style="width:
+                            100%;">Enviar</button>
+                    </div>
+                </form>
+        </div>
+        <script src="../script/contato-script.js"></script>
+
 </body>
+
 </html>
